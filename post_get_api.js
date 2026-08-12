691f2e77274327484ab52f13b6e4cd581b99fbe9
SaLiL-SinGh14 <salilsingh02@gmail.com>
2026-08-12 23:11:16 +0530

    post_get_api

--- post_get_api.js
+++ post_get_api.js
@@ -1,32 +1,70 @@
-const express=require("express")
-const app=express()
-const PORT=3000
+// const express = require("express")
+// const app = express()
+// const PORT = 3000
+
+// app.use(express.json())
+
+// const users = []
+
+// app.post("/register", (req, res) => {
+//     const user = {
+//         id: users.length + 1,
+//         ...req.body//means just spread/copy all the properties of this object as it is
+//     }
+//     users.push(user)
+//     res.send("User Registered")
+// })
+
+// app.get("/users", (req, res) => {
+//     res.json(users)
+// })
+
+// app.get("/users/:id", (req, res) => {
+//     const id = Number(req.params.id)
+
+//     const user = users.find(u => u.id === id)
+
+//     res.json(user)
+// })
+// //this code will give the user of specific id 
+// app.listen(PORT, () => {
+//     console.log("Server Started");
+// })
+//.......................................................................
+////what if user is not found in the array , so we will use res.status along with the msg 
+const express = require("express")
+const app = express()
+const PORT = 3000
 
 app.use(express.json())
 
-const users=[]
+const users = []
 
-app.post("/register",(req,res)=>{
-    const user={
-        id: users.length+1,
+app.post("/register", (req, res) => {
+    const user = {
+        id: users.length + 1,
         ...req.body//means just spread/copy all the properties of this object as it is
     }
     users.push(user)
-    res.send("User Registered")
+    res.status(201).send(user)
 })
 
-app.get("/users",(req,res)=>{
+app.get("/users", (req, res) => {
     res.json(users)
 })
 
-app.get("/users/:id",(req,res)=>{
-    const id=Number(req.params.id)
+app.get("/users/:id", (req, res) => {
+    const id = Number(req.params.id)
 
-    const user=users.find(u=>u.id===id)
+    const user = users.find(u => u.id === id)
+
+    if(!user){
+        return res.status(404).send("User not found");
+    }
 
     res.json(user)
 })
 //this code will give the user of specific id 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log("Server Started");
 })
